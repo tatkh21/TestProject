c8bb06da7d36586ead6fcf02230807c333e54cf7
tatyana <tatkh21@gmail.com>
2021-05-25 22:56:45 -0500

    First commit, office hours

---
+++ src/office_hours/practice_05_25_2021/Pangram.java
@@ -0,0 +1,30 @@
+package office_hours.practice_05_25_2021;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+public class Pangram {
+    public static void main(String[] args) {
+        String s = "A quick brown fox jumps over the lazy dog.";
+//        char [] a = s.replace(" ", "").toLowerCase().toCharArray();
+//        Arrays.sort(a);
+//        System.out.println(Arrays.toString(a));
+        System.out.println(isPangram(s));
+    }
+    public static boolean isPangram (String str) {
+        str = str.toLowerCase();
+        String alp = "abcdefjhigklmnopqrstuvwxyz";
+        for (int i = 0; i < str.length() ; i++) {
+
+            if (Character.isLetter(str.charAt(i))) {
+                alp = alp.replace("" + str.charAt(i), "");
+                System.out.println(str.charAt(i) + alp);
+            }
+            if (alp.isEmpty()) {
+                break;
+            }
+        }
+        return alp.isEmpty();
+
+    }
+}
